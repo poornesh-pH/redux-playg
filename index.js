2fa32cf2f8189761d49f40981b314bb1c001b4f0
poornesh-pH <poornesh9626@gmail.com>
2021-06-02 11:10:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,37 +6,48 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const store = createStore((state = { count: 0 }, action) => {
-
-switch(action.type){
-  case 'INCREMENT':
-  return{
-    count: state.count + 1
+  switch (action.type) {
+    case 'INCREMENT':
+      const incrementBy =
+        typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
   }
-  case 'DECREMENT':
-  return{
-    count: state.count - 1
-  }
-  case 'RESET':
-  return{
-    count : 0
-  }
-  default: return state
-}
 });
 
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 10
+});
 store.dispatch({
   type: 'INCREMENT'
 });
 store.dispatch({
   type: 'INCREMENT'
 });
+
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'DECREMENT'
+});
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 10
 });
 
 store.dispatch({
-  type:"DECREMENT"
-}
-)
+  type: 'SET',
+  count: 100
+});
 
 console.log(store.getState());
