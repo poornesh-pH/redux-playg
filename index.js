44a32996665179828d822f1327db9797b7c2d588
poornesh-pH <poornesh9626@gmail.com>
2021-06-04 08:28:29 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
+
+const demoState = {
+  expenses: [
+    {
+      id: 'asdasdasd',
+      amount: 1000,
+      note: 'This is a note',
+      description: 'This is the description',
+      createdAt: 0
+    }
+  ],
+  filters: {
+    sortBy: 'amount', // date or amount
+    text: 'rent',
+    startDate: undefined,
+    endDate: undefined
+  }
+};
+
+//ADD_EXPENSE
+const addExpense = ({
+  amount = 0,
+  note = '',
+  description = '',
+  createdAt = 0
+} = {}) => {
+  return {
+    type: 'ADDEXPENSE',
+    id: uuidv4(),
+    amount,
+    note,
+    description,
+    createdAt
+  };
+};
+//EDIT_EXPENSE
+//REMOVE_EXPENSE
+//SET_START_DATE
+//SET_END_DATE
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+
+const expenseReducerStateDefault = [];
+const expenseReducer = (state = expenseReducerStateDefault, action) => {
+  switch (action.type) {
+    case 'ADDEXPENSE':
+      return [...state, action.expenses];
+    default:
+      return state;
+  }
+};
+const filterReducerStateDefault = {
+  sortBy: 'amount',
+  text: 'rent',
+  startDate: undefined,
+  endDate: undefined
+};
+const filterReducer = (state = filterReducerStateDefault, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+const store = createStore(
+  combineReducers({
+    expenses: expenseReducer,
+    filters: filterReducer
+  })
+);
+
+store.dispatch(
+  addExpense({
+    description: 'rent',
+    amount: 2000,
+    note: 'this is rent',
+    createdAt: 0
+  })
+);
+
+console.log(store.getState());
